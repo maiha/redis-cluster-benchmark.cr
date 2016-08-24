144b890ba0cc8069c74c78bdd103e827801174aa
maiha <maiha@wota.jp>
2016-08-24 21:54:16 +0900

    spec

---
+++ spec/commands_spec.cr
@@ -0,0 +1,42 @@
+require "./spec_helper"
+
+private def parse1(str, keyspace = 10000)
+  cmds = Bench::Commands.parse(str, keyspace)
+  cmds.size.should eq(1)
+  return cmds.first
+end
+
+describe Bench::Commands do
+  describe ".parse" do
+    it "get x" do
+      cmd = parse1("get x")
+      10.times do
+        cmd.feed.should eq(["get", "x"])
+      end
+    end
+
+    it "get x__rand_int__" do
+      cmd = parse1("get x__rand_int__")
+      args = cmd.feed
+      args.size.should eq(2)
+      args[0].should eq("get")
+      (args[1] =~ /^x\d+$/).should be_a(Int32)
+    end
+
+    it "set __rand_int__ __rand_int__" do
+      cmd = parse1("set __rand_int__ __rand_int__")
+      args = cmd.feed
+      args.size.should eq(3)
+      args[0].should eq("set")
+      (args[1] =~ /^\d+$/).should be_a(Int32)
+      (args[2] =~ /^\d+$/).should be_a(Int32)
+    end
+
+    it "get __rand_int__ (with keyspace=1)" do
+      cmd = parse1("get __rand_int__", 1)
+      10.times do
+        cmd.feed.should eq(["get", "0"])
+      end
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/redis-cluster-benchmark"
+require "../src/bench"
